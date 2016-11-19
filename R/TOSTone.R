5db941219fadaa7dfaf5fad7b548b0054b40cb32
Daniel Lakens <Lakens@gmail.com>
2016-11-19 20:59:34 +0100

    initial commit

---
+++ R/TOSTone.R
@@ -0,0 +1,48 @@
+#' TOST function
+#' @param m mean
+#' @param mu value to compare against
+#' @param sd standard deviation
+#' @param n sample size
+#' @param low_eqbound_d lower equivalence bounds (e.g., -0.5) expressed in standardized mean difference (Cohen's d)
+#' @param high_eqbound_d upper equivalence bounds (e.g., 0.5) expressed in standardized mean difference (Cohen's d)
+#' @param alpha alpha level (default = 0.05)
+#' @return Returns dataframe with t-value, degrees of freedom, p-value, and lower and upper limit of confidence interval
+#' @examples
+#' TOST(m=0.54,mu=0.5,sd=1.2,n=100,low_eqbound_d=-0.3, high_eqbound_d=0.3, alpha=0.05)
+#' @export
+
+TOST<-function(m,mu,sd,n,low_eqbound_d, high_eqbound_d, alpha){
+  if(missing(alpha)) {
+    alpha<-0.05
+  }
+  low_eqbound<-low_eqbound_d*sd
+  high_eqbound<-high_eqbound_d*sd
+  t1<-(abs(m-mu)-high_eqbound)/(sd/sqrt(n)) #t-test
+  degree_f<-n-1
+  p1<-pt(t1, degree_f, lower=TRUE) 
+  t2<-(abs(m-mu)-low_eqbound)/(sd/sqrt(n))# t-test
+  p2<-pt(t2, degree_f, lower=FALSE) 
+  t<-(m-mu)/(sd/sqrt(n))
+  pttest<-2*pt(-abs(t), df=degree_f)
+  LL90<-(m-mu)-qt(1-alpha, degree_f)*(sd/sqrt(n))
+  UL90<-(m-mu)+qt(1-alpha, degree_f)*(sd/sqrt(n))
+  LL95<-(m-mu)-qt(1-(alpha/2), degree_f)*(sd/sqrt(n))
+  UL95<-(m-mu)+qt(1-(alpha/2), degree_f)*(sd/sqrt(n))
+  ptost<-max(p1,p2) #Get highest p-value for summary TOST result
+  ttost<-ifelse(abs(t1) < abs(t2), t1, t2) #Get lowest t-value for summary TOST result
+  results<-data.frame(ttost,degree_f,ptost,LL90,UL90)
+  dif<-(m-mu)
+  testoutcome<-ifelse(pttest<0.05,"significant","non-significant")
+  TOSToutcome<-ifelse(ptost<0.05,"significant","non-significant")
+  plot(NA, ylim=c(0,1), xlim=c(min(LL95,low_eqbound,dif)-max(UL95-LL95, high_eqbound-low_eqbound,dif)/10, max(UL95,high_eqbound,dif)+max(UL95-LL95, high_eqbound-low_eqbound, dif)/10), bty="l", yaxt="n", ylab="",xlab="Mean Difference")
+  points(x=dif, y=0.5, pch=15, cex=2)
+  abline(v=high_eqbound, lty=2)
+  abline(v=low_eqbound, lty=2)
+  abline(v=0, lty=2, col="grey")
+  segments(LL90,0.5,UL90,0.5, lwd=3)
+  segments(LL95,0.5,UL95,0.5, lwd=1)
+  title(main=paste("Equivalence bounds ",round(low_eqbound,digits=3)," and ",round(high_eqbound,digits=3),"\nMean difference = ",round(dif,digits=3)," \n TOST: 90% CI [",round(LL90,digits=3),";",round(UL90,digits=3),"] ", TOSToutcome," \n NHST: 95% CI [",round(LL95,digits=3),";",round(UL95,digits=3),"] ", testoutcome,sep=""), cex.main=1)
+  message(cat("Student's t-test was ",testoutcome,", t(",degree_f,") = ",t,", p = ",pttest,sep=""))
+  message(cat("The equivalence test based on Student's t-test was ",TOSToutcome,", t(",degree_f,") = ",ttost,", p = ",ptost,sep=""))
+  return(results)
+}
