35a61b39d1f28067a7357203e6799a826ce2454a
Daniel Lakens <Lakens@gmail.com>
2017-10-02 22:27:26 +0200

    add power calculated given N for one-sample t-test

--- R/powerTOSTone.R
+++ R/powerTOSTone.R
@@ -3,21 +3,36 @@
 #' @param statistical_power desired power (e.g., 0.8)
 #' @param low_eqbound_d lower equivalence bounds (e.g., -0.5) expressed in standardized mean difference (Cohen's d)
 #' @param high_eqbound_d upper equivalence bounds (e.g., 0.5) expressed in standardized mean difference (Cohen's d)
-#' @return Returns a string summarizing the power analysis, and a numeric variable for the number of observations needed in each group
-#' @examples 
-#' ## Sample size for alpha = 0.05, 90% power, equivalence bounds of 
+#' @return Returns a string summarizing the power analysis, and a numeric variable for power or number of observations needed in each group
+#' @examples
+#' ## Sample size for alpha = 0.05, 90% power, equivalence bounds of
 #' ## Cohen's d = -0.3 and Cohen's d = 0.3, and assuming true effect = 0
 #' powerTOSTone(alpha=0.05, statistical_power=0.9, low_eqbound_d=-0.3, high_eqbound_d=0.3)
+#'
+#' ## Power for sample size of 121, alpha = 0.05, equivalence bounds of
+#' ## Cohen's d = -0.3 and Cohen's d = 0.3, and assuming true effect = 0
+#'
+#' powerTOSTone(alpha=0.05, N=121, low_eqbound_d=-0.3, high_eqbound_d=0.3)
 #' @section References:
 #' Chow, S.-C., Wang, H., & Shao, J. (2007). Sample Size Calculations in Clinical Research, Second Edition - CRC Press Book. Formula 3.1.9
 #' @importFrom stats pnorm pt qnorm qt
 #' @importFrom graphics abline plot points segments title
 #' @export
 
-powerTOSTone<-function(alpha, statistical_power, low_eqbound_d, high_eqbound_d){
-  NT1<-(qnorm(1-alpha)+qnorm(1-((1-statistical_power)/2)))^2/(low_eqbound_d)^2
-  NT2<-(qnorm(1-alpha)+qnorm(1-((1-statistical_power)/2)))^2/(high_eqbound_d)^2
-  N<-ceiling(max(NT1,NT2))
-  message(cat("The required sample size to achieve",100*statistical_power,"% power with equivalence bounds of",low_eqbound_d,"and",high_eqbound_d,"is",N))
-  return(N)
+powerTOSTone<-function(alpha, statistical_power, N, low_eqbound_d, high_eqbound_d){
+  if(missing(N)) {
+    NT1<-(qnorm(1-alpha)+qnorm(1-((1-statistical_power)/2)))^2/(low_eqbound_d)^2
+    NT2<-(qnorm(1-alpha)+qnorm(1-((1-statistical_power)/2)))^2/(high_eqbound_d)^2
+    N<-ceiling(max(NT1,NT2))
+    message(cat("The required sample size to achieve",100*statistical_power,"% power with equivalence bounds of",low_eqbound_d,"and",high_eqbound_d,"is",N))
+    return(N)
+  }
+  if(missing(statistical_power)) {
+    statistical_power1<-2*(pnorm((abs(low_eqbound_d)*sqrt(N))-qnorm(1-alpha))+pnorm(-(abs(low_eqbound_d)*sqrt(N))-qnorm(1-alpha)))-1
+    statistical_power2<-2*(pnorm((abs(high_eqbound_d)*sqrt(N))-qnorm(1-alpha))+pnorm(-(abs(high_eqbound_d)*sqrt(N))-qnorm(1-alpha)))-1
+    statistical_power<-min(statistical_power1,statistical_power2)
+    if(statistical_power<0) {statistical_power<-0}
+    message(cat("The statistical power is",round(100*statistical_power,2),"% for equivalence bounds of",low_eqbound_d,"and",high_eqbound_d,"."))
+    return(statistical_power)
+  }
 }
