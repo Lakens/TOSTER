062d947edc3935222c6e633636f6af94c7d0eb2a
Daniel Lakens <Lakens@gmail.com>
2017-10-15 09:16:17 +0200

    fixed power example

--- R/powerTOSTtwo.raw.R
+++ R/powerTOSTtwo.raw.R
@@ -12,9 +12,9 @@
 #' ## units, assuming pooled standard deviation of 350, and assuming true effect = 0
 #' powerTOSTtwo.raw(alpha=0.05,statistical_power=0.8,low_eqbound=-200,high_eqbound=200,sdpooled=350)
 #'
-#' ## Sample size for alpha = 0.05, N = 53 per group, equivalence bounds of
+#' ## Power for alpha = 0.05, N = 53 per group, equivalence bounds of
 #' ## -200 and 200 in raw units, assuming sdpooled = 350 and true effect = 0
-#' powerTOSTtwo.raw(alpha=0.05,statistical_power=0.8,low_eqbound=-200,high_eqbound=200,sdpooled=350)
+#' powerTOSTtwo.raw(alpha=0.05,N = 53,low_eqbound=-200,high_eqbound=200,sdpooled=350)
 #'
 #' ## Equivalence bounds for alpha = 0.05, N = 108 per group, statistical power of
 #' ## 0.8, assuming true effect = 0
