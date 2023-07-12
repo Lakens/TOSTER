84d3483868ffeed34c008613c13ececfebe21d57
arcaldwell49 <arcaldwell49@gmail.com>
2023-07-12 17:49:01 -0400

    Fixing two sample permutation test

--- R/brunner_munzel.R
+++ R/brunner_munzel.R
@@ -5,7 +5,9 @@
 #' @param formula a formula of the form lhs ~ rhs where lhs is a numeric variable giving the data values and rhs either 1 for a one-sample or paired test or a factor with two levels giving the corresponding groups. If lhs is of class "Pair" and rhs is 1, a paired test is done.
 #' @param data an optional matrix or data frame (or similar: see model.frame) containing the variables in the formula formula. By default the variables are taken from environment(formula).
 #' @param paired a logical indicating whether you want a paired test.
-#' @param p_null 	a number specifying an optional parameter used to form the null hypothesis (Default = 0.5). This can be thought of as the null in terms of probability of exchangeability, p = P (X < Y ) + 0.5 * P (X = Y); See ‘Details’.
+#' @param p_null 	a number specifying an optional parameter used to form the null hypothesis (Default = 0.5). This can be thought of as the null in terms of probability of superiority, p = P (X < Y ) + 0.5 * P (X = Y); See ‘Details’.
+#' @param perm a logical indicating whether or not to perform a permutation test over approximate t-distribution based test (default is FALSE). Highly recommend to set perm = TRUE when sample size per condition is less than 15.
+#' @param max_n_perm the maximum number of permutations (default is 10000).
 #' @param alternative a character string specifying the alternative hypothesis, must be one of "two.sided" (default), "greater" or "less". You can specify just the initial letter.
 #' @param subset an optional vector specifying a subset of observations to be used.
 #' @param na.action a function which indicates what should happen when the data contain NAs. Defaults to getOption("na.action").
@@ -13,9 +15,14 @@
 #' @param ...  further arguments to be passed to or from methods.
 #' @details
 #'
-#' The estimate of probability of exchangeability refers to the following:
+#' The estimate of probability of superiority refers to the following:
 #'
-#'  p(X<Y) + .5*P(X=Y)
+#'  p = p(X<Y) + .5*P(X=Y)
+#'
+#'  Note, for paired samples, this does *not* refer to the probability of an increase/decrease in paired sample but rather the probability that a randomly sampled value of X.
+#'  This is also referred to as the "relative" effect in the literature. Therefore, the results will differ from the concordance probability provided by the ses_calc function.
+#'
+#'  The brunner_munzel function is based on the npar.t.test and npar.t.test.paired functions within the nparcomp package (Konietschke et al. 2015).
 #'
 #' @return A list with class \code{"htest"} containing the following components:
 #' \describe{
@@ -40,6 +47,8 @@
 #' Neubert, K., Brunner, E., (2006). A Studentized Permutation Test for the Nonparametric Behrens-Fisher Problem. Computational Statistics and Data Analysis.
 #'
 #' Munzel, U., Brunner, E. (2002). An Exact Paired Rank Test. Biometrical Journal 44, 584-593.
+#'
+#' Konietschke, F., Placzek, M., Schaarschmidt, F., & Hothorn, L. A. (2015). nparcomp: an R software package for nonparametric multiple comparisons and simultaneous confidence intervals. Journal of Statistical Software 64 (2015), Nr. 9, 64(9), 1-17. http://www.jstatsoft.org/v64/i09/
 #' @family TOST
 #' @name brunner_munzel
 #' @export brunner_munzel
@@ -51,6 +60,8 @@ brunner_munzel <- function(x,
                                            "greater"),
                            p_null = 0.5,
                            alpha = 0.05,
+                           perm = FALSE,
+                           max_n_perm = 10000,
                            ...) {
 
   UseMethod("brunner_munzel")
@@ -70,6 +81,8 @@ brunner_munzel.default = function(x,
                                                   "greater"),
                                   p_null = 0.5,
                                   alpha = 0.05,
+                                  perm = FALSE,
+                                  max_n_perm = 10000,
                                   ...
 ) {
   alternative = match.arg(alternative)
@@ -94,17 +107,27 @@ brunner_munzel.default = function(x,
       x <- x[is.finite(x)]
       y <- y[is.finite(y)]
     }
+
+    if(min(length(x),length(y)) < 15 && !(perm)){
+      message("Sample size in at least one group is small. Permutation test (perm = TRUE) is highly recommended.")
+    }
+    if(min(length(x),length(y)) > 250 &&
+       sum(length(x),length(y)) > 500 &&
+       (perm)){
+      message("Sample size is fairly large. Use of a permutation test is probably unnessary.")
+    }
   } else {
 
     stop("'y' is missing. One sample tests currently not supported.")
 
   }
+
+
   # Paired -----
   if(paired){
 
-    METHOD = "Exact Paired Brunner-Munzel test"
     n = length(x)
-
+    n1 = n+1
     df.sw = n - 1
     all_data <- c(y, x)
     N = length(all_data)
@@ -129,24 +152,92 @@ brunner_munzel.default = function(x,
     v0 <- (v == 0)
     v[v0] <- 1 / n
     test_stat <- sqrt(n) * (pd - p_null) / sqrt(v)
+    std_err = sqrt(v)
 
-    p.value = switch(alternative,
-                     "two.sided" = (2*min(pt(test_stat,n-1),
-                                          1-pt(test_stat,n-1))),
-                     "less" = pt(test_stat,
-                                 df=df.sw),
-                     "greater" =  1-pt(test_stat,
-                                       df=df.sw))
+    if(perm == TRUE){
+      METHOD = "Paired Brunner-Munzel permutation test"
+      # Directly from nparcomp
+      if(n<=13){
+        max_n_perm=2^n
+        p<-0
+        for (i in 1:n){
+          a<-rep(c(rep(c(i,i+n),max_n_perm/(2^i)),rep(c(i+n,i),max_n_perm/(2^i))),2^(i-1))
+          p<-rbind(p,a)
+        }
+        p<-p[2:(n+1),]
+        P<-matrix(p,ncol=max_n_perm)
 
-    pd.lower = switch(alternative,
-                      "two.sided" = pd-qt(1-alpha/2,df.sw)*sqrt(v/n),
-                      "less" = 0,
-                      "greater" =  pd-qt(1-alpha,df.sw)*sqrt(v/n))
+        xperm<-matrix(all_data[P],nrow=N,ncol=max_n_perm)
+        rxperm<-matrix(rx[P],nrow=N,ncol=max_n_perm)
+      }
+      else{
+        P<-matrix(nrow=n,ncol=10000)
+        permu<-function(all_data){
+          n<-length(all_data)
+          result<-sample(c(0,1),size=n,replace=TRUE)
+          return(result)
+        }
+        P1<-apply(P,2,permu)
+        P2<-rbind(P1,P1)
+        xperm<-all_data*P2+xinverse*(1-P2)
+        rxperm<-rx*P2+rxinverse*(1-P2)
+      }
+      xperm1<-xperm[1:n,]
+      xperm2<-xperm[n1:N,]
+      rperm1<-rxperm[1:n,]
+      rperm2<-rxperm[n1:N,]
+      riperm1<-apply(xperm1,2,rank)
+      riperm2<-apply(xperm2,2,rank)
+      BMperm2<-1/n*(rperm2-riperm2)
+      BMperm3<-1/n*(rperm1-riperm1)-BMperm2
+      pdperm<-colMeans(BMperm2)
+      mperm3<-colMeans(BMperm3)
+      vperm3<-(colSums(BMperm3^2)-n*mperm3^2)/(n-1)
+      vperm30<-(vperm3==0)
+      vperm3[vperm30]<-1/n
+      #print(vperm3)
+      Tperm<-sqrt(n)*(pdperm-p_null)/sqrt(vperm3)
+      p1perm<-mean(Tperm<=test_stat)
+      pq1<-sort(Tperm)[(floor((1-alpha/2)*max_n_perm)+1)]
+      pq2<-sort(Tperm)[(floor((1-alpha)*max_n_perm)+1)]
 
-    pd.upper = switch(alternative,
-                      "two.sided" = pd+qt(1-alpha/2,df.sw)*sqrt(v/n),
-                      "less" = pd+qt(1-alpha,df.sw)*sqrt(v/n),
-                      "greater" =  1)
+      p.value = switch(alternative,
+                       "two.sided" = min(2*p1perm,2*(1-p1perm)),
+                       "less" = p1perm,
+                       "greater" =  1-p1perm)
+
+      pd.lower = switch(alternative,
+                        "two.sided" = pd-pq1*sqrt(v/n),
+                        "less" = 0,
+                        "greater" =  pd-pq2*sqrt(v/n))
+
+      pd.upper = switch(alternative,
+                        "two.sided" = pd+pq1*sqrt(v/n),
+                        "less" = pd+pq2*sqrt(v/n),
+                        "greater" =  1)
+    } else {
+
+      METHOD = "exact paired Brunner-Munzel test"
+      p.value = switch(alternative,
+                       "two.sided" = (2*min(pt(test_stat,n-1),
+                                            1-pt(test_stat,n-1))),
+                       "less" = pt(test_stat,
+                                   df=df.sw),
+                       "greater" =  1-pt(test_stat,
+                                         df=df.sw))
+
+      pd.lower = switch(alternative,
+                        "two.sided" = pd-qt(1-alpha/2,df.sw)*sqrt(v/n),
+                        "less" = 0,
+                        "greater" =  pd-qt(1-alpha,df.sw)*sqrt(v/n))
+
+      pd.upper = switch(alternative,
+                        "two.sided" = pd+qt(1-alpha/2,df.sw)*sqrt(v/n),
+                        "less" = pd+qt(1-alpha,df.sw)*sqrt(v/n),
+                        "greater" =  1)
+    }
+
+
 
   } else{
 
@@ -168,17 +259,63 @@ brunner_munzel.default = function(x,
     s1 <- var(pl2)/n.x
     s2 <- var(pl1)/n.y
 
-    V <- N*(s1 +s2)
+    V <- N*(s1+s2)
     singular.bf <- (V == 0)
     V[singular.bf] <- N/(2 * n.x * n.y)
+    std_err = sqrt(V)
 
 
-    ## nparcomp ------
     test_stat <- sqrt(N)*(pd - p_null)/sqrt(V)
     df.sw <- (s1 + s2)^2/(s1^2/(n.x - 1) + s2^2/(n.y - 1))
     df.sw[is.nan(df.sw)] <- 1000
-    METHOD = "Two Sample Brunner-Munzel test"
 
+    if(perm){
+
+      ## permutation -----
+      METHOD = "two-sample Brunner-Munzel permutation test"
+
+      Tprob<-qnorm(pd)*exp(-0.5*qnorm(pd)^2)*sqrt(N/(V*2*pi))
+      P<-apply(matrix(rep(1:N,max_n_perm),ncol=max_n_perm),2,sample)
+      Px<-matrix(c(x,y)[P],ncol=max_n_perm)
+      Tperm<-t(apply(perm_loop(Px[1:n.x,],Px[(n.x+1):N,],n.x,n.y,max_n_perm),1,sort))
+      p.PERM1<-mean((test_stat <= Tperm[1,]))
+      if(alternative == "two.sided"){
+        c1<-0.5*(Tperm[1,floor((1-alpha/2)*max_n_perm)]+Tperm[1,ceiling((1-alpha/2)*max_n_perm)])
+        c2<-0.5*(Tperm[1,floor(alpha/2*max_n_perm)]+Tperm[1,ceiling(alpha/2*max_n_perm)])
+      } else {
+        c1<-0.5*(Tperm[1,
+                       floor((1-alpha)*max_n_perm)]+Tperm[1,
+                                                          ceiling((1-alpha)*max_n_perm)])
+        c2<-0.5*(Tperm[1,
+                       floor(alpha*max_n_perm)]+Tperm[1,
+                                                      ceiling(alpha*max_n_perm)])
+      }
+
+      lower_ci = p-sqrt(V/N)*c1
+      upper_ci = p-sqrt(V/N)*c2
+
+      p.value = switch(alternative,
+                       "two.sided" = min(2-2*p.PERM1,
+                                         2*p.PERM1),
+                       "less" =  p.PERM1,
+                       "greater" =  1-p.PERM1)
+
+      pd.lower = switch(alternative,
+                        "two.sided" = lower_ci,
+                        "less" = 0,
+                        "greater" =  lower_ci)
+      pd.lower = ifelse(pd.lower < 0, 0, pd.lower)
+
+      pd.upper = switch(alternative,
+                        "two.sided" = upper_ci,
+                        "less" = upper_ci,
+                        "greater" =  1)
+      pd.upper = ifelse(pd.upper > 1, 1, pd.upper)
+
+      } else{
+
+        ## t-approx ----
+    METHOD = "two-sample Brunner-Munzel test"
     p.value = switch(alternative,
                      "two.sided" = min(c(2 - 2 * pt(test_stat,
                                                   df=df.sw),
@@ -198,9 +335,10 @@ brunner_munzel.default = function(x,
                       "less" = pd + qt(1-alpha, df=df.sw)/sqrt(N)*sqrt(V),
                       "greater" =  1)
     pd.upper = ifelse(pd.upper > 1, 1, pd.upper)
+    }
   }
 
-  names(p_null) <- "probability of exchangeability"
+  names(p_null) <- "probability of superiority"
   names(test_stat) = "t"
   names(df.sw) = "df"
   cint = c(pd.lower,pd.upper)
@@ -214,6 +352,7 @@ brunner_munzel.default = function(x,
                parameter = df.sw,
                p.value = as.numeric(p.value),
                estimate = estimate,
+               stderr = std_err,
                conf.int = cint,
                null.value = p_null,
                alternative = alternative,
@@ -255,5 +394,49 @@ brunner_munzel.formula = function(formula,
   y$data.name <- DNAME
   y
 
+}
+
+
+perm_loop <-function(x1,x2,n1,n2,max_n_perm){
+
+  pl1P<-matrix(0,nrow=n1,ncol=max_n_perm)
+  pl2P<-matrix(0,nrow=n2,ncol=max_n_perm)
+
+  for(h1 in 1:n1){
+    help1<-matrix(t(x1[h1,]),
+                  ncol=max_n_perm,
+                  nrow=n2,byrow=TRUE)
+    pl1P[h1,]<-1/n2*(colSums((x2<help1)+1/2*(x2==help1)))
+  }
+  for(h2 in 1:n2){
+    help2<-matrix(t(x2[h2,]),
+                  ncol=max_n_perm,
+                  nrow=n1,byrow=TRUE)
+    pl2P[h2,]<-1/n1*(colSums((x1<help2)+1/2*(x1==help2)))
+  }
+
+  pdP<-colMeans(pl2P)
+  pd2P<-colMeans(pl1P)
+
+  v1P<-(colSums(pl1P^2)-n1*pd2P^2)/(n1-1)
+  v2P<-(colSums(pl2P^2)-n2*pdP^2)/(n2-1)
+  vP<-v1P/n1 + v2P/n2
+
+  v0P<-(vP==0)
+  vP[v0P]<-0.5/(n1*n2)^2
+
+  res1<-matrix(rep(0,max_n_perm*3),nrow=3)
+
+  res1[1,]<-(pdP-1/2)/sqrt(vP)
+
+  pdP0<-(pdP==0)
+  pdP1<-(pdP==1)
+  pdP[pdP0]<-0.01
+  pdP[pdP1]<-0.99
+
+  res1[2,]<-log(pdP/(1-pdP))*pdP*(1-pdP)/sqrt(vP)
+  res1[3,]<-qnorm(pdP)*exp(-0.5*qnorm(pdP)^2)/(sqrt(2*pi*vP))
+
+  res1
 }
 
--- junk/test_brunner.R
+++ junk/test_brunner.R
@@ -9,9 +9,27 @@ test1_comp = npar.t.test(data = sleep2, extra ~ group, method = "t.app")
 summary(test1_comp)
 test1
 
-test2 = brunner_munzel(data = sleep, extra ~ group,
-                       paired = TRUE)
-wilcox.test(data = sleep, extra ~ group,
-               paired = TRUE)
+test2_t = brunner_munzel(data = sleep, extra ~ group,
+                       paired = TRUE,
+                       perm = FALSE)
+
+test2_p = brunner_munzel(data = sleep, extra ~ group,
+                       paired = TRUE,
+                       perm = TRUE)
+
 test2_comp = npar.t.test.paired(data = sleep2, extra ~ group)
 summary(test2_comp)
+
+npar.t.test.paired(data = sleep2, extra ~ group)
+
+
+test1_comp = npar.t.test(data = sleep2, extra ~ group, method = "permu",
+                         alternative = "two.sided",
+                         nperm = 20000)
+test1_comp$Analysis
+test1 = brunner_munzel(data = sleep,
+                       x=x,
+                       y=y,
+                       perm = TRUE,
+                       max_n_perm = 20000)
+test1
