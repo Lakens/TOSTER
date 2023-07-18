5055d4cd5aac4457ef000ce4f9854ec628ece657
arcaldwell49 <arcaldwell49@gmail.com>
2023-07-18 15:43:39 -0400

    Update two_proportions.R

--- R/two_proportions.R
+++ R/two_proportions.R
@@ -13,6 +13,11 @@ twoprop_test = function(p1, p2,
   prop_dif <- p1 - p2
   # proportion se
   prop_se <- sqrt((p1*(1-p1))/n1 + (p2*(1-p2))/n2)
+  YATES <- abs(prop_dif) / sum(1 / n1, 1 / n2)
+  if (any((null <= 0) | (null >= 1))) {
+    stop("elements of 'null' must be in (0,1)")
+  }
+
 
   if(alternative %in% c("equivalence","minimal.effect")){
 
@@ -43,6 +48,43 @@ twoprop_test = function(p1, p2,
                                   alternative = "greater")
     )
 
+    PVAL = switch(
+      alternative,
+      "equivalence" = max(lo_pvalue, hi_pvalue),
+      "minimal.effect" = min(lo_pvalue, hi_pvalue)
+    )
+
+    test_p = PVAL == c(lo_pvalue, hi_pvalue)
+    test_z = c(lo_ztest, hi_ztest)
+    ZTEST = test_z[test_p]
+    conf_level = 1-alpha*2
+    conf = 1-alpha
+
+  } else{
+    if(length(null) != 1){
+      stop("null must have length of 1 if alternative is not a TOST test.")
+    }
+    ZTEST = (prop_dif - null) / prop_se
+    PVAL = p_from_z(ZTEST,
+                    alternative = alternative)
+
+    conf_level = switch(
+      alternative,
+      "two.sided" = 1-alpha,
+      "less" =  1-alpha*2,
+      "greater" = 1-alpha*2
+    )
+
+    conf = switch(
+      alternative,
+      "two.sided" = 1-alpha/2,
+      "less" =  1-alpha,
+      "greater" = 1-alpha
+    )
+
   }
+
+  # NORMAL APPROX.
+  conf_int = prop_dif - c(-1,1)*(qnorm(conf) * prop_se)
 
 }
