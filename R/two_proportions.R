140847610886aa82ead2e511d826bff1228e49ce
arcaldwell49 <arcaldwell49@gmail.com>
2023-07-18 11:18:35 -0400

    Create two_proportions.R

---
+++ R/two_proportions.R
@@ -0,0 +1,48 @@
+
+
+twoprop_test = function(p1, p2,
+                        n1, n2,
+                        null = 0,
+                        alpha = .05,
+                        alternative = c("two.sided",
+                                        "less",
+                                        "greater",
+                                        "equivalence",
+                                        "minimal.effect")){
+
+  prop_dif <- p1 - p2
+  # proportion se
+  prop_se <- sqrt((p1*(1-p1))/n1 + (p2*(1-p2))/n2)
+
+  if(alternative %in% c("equivalence","minimal.effect")){
+
+    if (length(null) == 1) {
+      if (null ==  0) {
+        stop("null cannot be zero if alternative is equivalence or minimal.effect")
+      }
+      null = c(null,-1 * null)
+
+    }
+
+    lo_ztest = (prop_dif - min(null))/prop_se
+    hi_ztest = (prop_dif - max(null))/prop_se
+
+    lo_pvalue = switch(
+      alternative,
+      "equivalence" = p_from_z(lo_ztest,
+                               alternative = "greater"),
+      "minimal.effect" = p_from_z(lo_ztest,
+                                  alternative = "less")
+    )
+
+    hi_pvalue = switch(
+      alternative,
+      "equivalence" = p_from_z(hi_ztest,
+                               alternative = "less"),
+      "minimal.effect" = p_from_z(hi_ztest,
+                                  alternative = "greater")
+    )
+
+  }
+
+}
