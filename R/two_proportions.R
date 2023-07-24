beb2d8e0cb95fd0c5eb4c8318106a37a48b321c5
arcaldwell49 <arcaldwell49@gmail.com>
2023-07-24 10:05:15 -0400

    Update two_proportions.R

--- R/two_proportions.R
+++ R/two_proportions.R
@@ -21,7 +21,7 @@
 
 twoprop_test = function(p1, p2,
                         n1, n2,
-                        null = 0,
+                        null,
                         alpha = .05,
                         alternative = c("two.sided",
                                         "less",
@@ -56,28 +56,28 @@ twoprop_test = function(p1, p2,
   }
 
   # effect size ----
-  effect_size = switch(effect_size,
-                       difference = calc_prop_dif(p1, p2, n1, n2,
-                                                  conf_level,
-                                                  conf,
-                                                  alternative),
-                       odds.ratio = calc_odds_ratio(p1, p2, n1, n2,
-                                                    conf_level,
-                                                    conf,
-                                                    alternative),
-                       risk.ratio = calc_risk_ratio(p1, p2, n1, n2,
-                                                    conf_level,
-                                                    conf,
-                                                    alternative))
+  res_tests = switch(effect_size,
+                       difference = test_prop_dif(p1, p2, n1, n2,
+                                                  null,
+                                                  alternative,
+                                                  alpha),
+                       odds.ratio = test_odds_ratio(p1, p2, n1, n2,
+                                                    null,
+                                                    alternative,
+                                                    alpha),
+                       risk.ratio = test_risk_ratio(p1, p2, n1, n2,
+                                                    null,
+                                                    alternative,
+                                                    alpha))
 
 
   METHOD = "difference in two proportions z-test"
-  RVAL <- list(statistic = STATISTIC,
+  RVAL <- list(statistic = res_tests$STATISTIC,
                #parameter = PARAMETER,
-               p.value = as.numeric(PVAL),
-               estimate = effect_size$ESTIMATE,
-               null.value = NVAL,
-               conf.int = effect_size$CINT,
+               p.value = as.numeric(res_tests$PVAL),
+               estimate = res_tests$ESTIMATE,
+               null.value = res_tests$NVAL,
+               conf.int = res_tests$CINT,
                alternative = alternative,
                method = METHOD)
   class(RVAL) <- "htest"
@@ -87,7 +87,11 @@ twoprop_test = function(p1, p2,
 
 test_prop_dif = function(p1,p2,n1,n2,
                          null,
-                         alternative) {
+                         alternative,
+                         alpha) {
+  if(is.null(null)){
+    null = 0
+  }
 
   prop_dif <- p1 - p2
   # proportion se
@@ -135,6 +139,9 @@ test_prop_dif = function(p1,p2,n1,n2,
     test_p = PVAL == c(lo_pvalue, hi_pvalue)
     test_z = c(lo_ztest, hi_ztest)
     ZTEST = test_z[test_p]
+
+    conf_level = 1-alpha*2
+    conf = 1-alpha
   } else{
     if(length(null) != 1){
       stop("null must have length of 1 if alternative is not a TOST test.")
@@ -142,7 +149,35 @@ test_prop_dif = function(p1,p2,n1,n2,
     ZTEST = (prop_dif - null) / prop_se
     PVAL = p_from_z(ZTEST,
                     alternative = alternative)
+
+    conf_level = switch(
+      alternative,
+      "two.sided" = 1-alpha,
+      "less" =  1-alpha*2,
+      "greater" = 1-alpha*2
+    )
+
+    conf = switch(
+      alternative,
+      "two.sided" = 1-alpha/2,
+      "less" =  1-alpha,
+      "greater" = 1-alpha
+    )
   }
+
+  z_mult = qnorm(conf)
+  ESTIMATE = prop_dif
+  names(ESTIMATE) = "difference in proportions"
+
+  CINT = prop_dif - c(-1,1)*(qnorm(conf) * prop_se)
+  if(alternative == "less"){
+    CINT[1] = -Inf
+  }
+
+  if(alternative == "greater"){
+    CINT[2] = Inf
+  }
+  attr(CINT, "conf.level") <- conf_level
 
   STATISTIC = ztest
   names(STATISTIC) <- "z"
@@ -152,16 +187,20 @@ test_prop_dif = function(p1,p2,n1,n2,
 
   list(STATISTIC = STATISTIC,
        PVAL = PVAL,
-       NVAL = NVAL)
+       NVAL = NVAL,
+       ESTIMATE = ESTIMATE,
+       CINT = CINT)
 }
 
 
 
-calc_odds_ratio = function(p1,p2,n1,n2,
-                           conf_level = .95,
-                           conf = .975,
-                           alternative){
-
+test_odds_ratio = function(p1, p2, n1, n2,
+                           null,
+                           alternative,
+                           alpha){
+  if(is.null(null)){
+    null = 1
+  }
   # Fleiss, J. L., Levin, B., Paik, M.C. 2003. Statistical Methods for Rates and Proportions. Third Edition. John Wiley & Sons. New York.
   q1 = 1-p1
   q2 = 1-p2
@@ -169,7 +208,72 @@ calc_odds_ratio = function(p1,p2,n1,n2,
   b = 1/(n1*q1+.5)
   c = 1/(n2*p2+.5)
   d = 1/(n2*q2+.5)
+  m1 = n1*p1 + n2*p2
   OR = (p1/q1) / (p2/q2)
+
+  if(alternative %in% c("equivalence","minimal.effect")){
+
+    if (length(null) == 1) {
+      if (null ==  1) {
+        stop("null for odds ratio cannot be 1 if alternative is equivalence or minimal.effect")
+      }
+      null = c(null,-1 * null)
+
+    }
+
+    lo_ztest = (prop_dif - min(null))/prop_se
+    hi_ztest = (prop_dif - max(null))/prop_se
+
+    lo_pvalue = switch(
+      alternative,
+      "equivalence" = p_from_z(lo_ztest,
+                               alternative = "greater"),
+      "minimal.effect" = p_from_z(lo_ztest,
+                                  alternative = "less")
+    )
+
+    hi_pvalue = switch(
+      alternative,
+      "equivalence" = p_from_z(hi_ztest,
+                               alternative = "less"),
+      "minimal.effect" = p_from_z(hi_ztest,
+                                  alternative = "greater")
+    )
+
+    PVAL = switch(
+      alternative,
+      "equivalence" = max(lo_pvalue, hi_pvalue),
+      "minimal.effect" = min(lo_pvalue, hi_pvalue)
+    )
+
+    test_p = PVAL == c(lo_pvalue, hi_pvalue)
+    test_z = c(lo_ztest, hi_ztest)
+    ZTEST = test_z[test_p]
+
+    conf_level = 1-alpha*2
+    conf = 1-alpha
+  } else{
+    if(length(null) != 1){
+      stop("null must have length of 1 if alternative is not a TOST test.")
+    }
+    ZTEST = (prop_dif - null) / prop_se
+    PVAL = p_from_z(ZTEST,
+                    alternative = alternative)
+
+    conf_level = switch(
+      alternative,
+      "two.sided" = 1-alpha,
+      "less" =  1-alpha*2,
+      "greater" = 1-alpha*2
+    )
+
+    conf = switch(
+      alternative,
+      "two.sided" = 1-alpha/2,
+      "less" =  1-alpha,
+      "greater" = 1-alpha
+    )
+  }
 
   z_mult = qnorm(conf)
   se_logodds = sqrt(sum(a,b,c,d))
@@ -195,16 +299,94 @@ calc_odds_ratio = function(p1,p2,n1,n2,
 
 }
 
-calc_risk_ratio = function(p1,p2,n1,n2,
-                           conf_level = .95,
-                           conf = .975,
-                           alternative){
+miettinen_nurminen_z = function(p1, p2,
+                                n1, n2,
+                                m1, null){
+
+  a_parameter = n2*(null-1)
+  b_parameter = n1*null+n2-m1*(null-1)
+  c_parameter = -1*m1
+
+
+  p_tilde2 = (-1*b_parameter + sqrt(b_parameter^2-4*a_parameter*c_parameter) ) / (2*a_parameter)
+
+  p_tilde1 = (p_tilde2 * null)/ (1+ p_tild2*(null-1))
+}
+
+test_risk_ratio = function(p1, p2, n1, n2,
+                           null,
+                           alternative,
+                           alpha){
   # Gart and Nam (1988), page 324
   # Gart, John J. and Nam, Jun-mo. 1988. 'Approximate Interval Estimation of the Ratio of Binomial Parameters: Review and Corrections for Skewness.' Biometrics, Volume 44, 323-338
   phi = p1/p2
   z_mult = qnorm(conf)
   q1 = 1-p1
   q2 = 1-p2
+
+  if(alternative %in% c("equivalence","minimal.effect")){
+
+    if (length(null) == 1) {
+      if (null ==  0) {
+        stop("null cannot be zero if alternative is equivalence or minimal.effect")
+      }
+      null = c(null,-1 * null)
+
+    }
+
+    lo_ztest = (prop_dif - min(null))/prop_se
+    hi_ztest = (prop_dif - max(null))/prop_se
+
+    lo_pvalue = switch(
+      alternative,
+      "equivalence" = p_from_z(lo_ztest,
+                               alternative = "greater"),
+      "minimal.effect" = p_from_z(lo_ztest,
+                                  alternative = "less")
+    )
+
+    hi_pvalue = switch(
+      alternative,
+      "equivalence" = p_from_z(hi_ztest,
+                               alternative = "less"),
+      "minimal.effect" = p_from_z(hi_ztest,
+                                  alternative = "greater")
+    )
+
+    PVAL = switch(
+      alternative,
+      "equivalence" = max(lo_pvalue, hi_pvalue),
+      "minimal.effect" = min(lo_pvalue, hi_pvalue)
+    )
+
+    test_p = PVAL == c(lo_pvalue, hi_pvalue)
+    test_z = c(lo_ztest, hi_ztest)
+    ZTEST = test_z[test_p]
+
+    conf_level = 1-alpha*2
+    conf = 1-alpha
+  } else{
+    if(length(null) != 1){
+      stop("null must have length of 1 if alternative is not a TOST test.")
+    }
+    ZTEST = (prop_dif - null) / prop_se
+    PVAL = p_from_z(ZTEST,
+                    alternative = alternative)
+
+    conf_level = switch(
+      alternative,
+      "two.sided" = 1-alpha,
+      "less" =  1-alpha*2,
+      "greater" = 1-alpha*2
+    )
+
+    conf = switch(
+      alternative,
+      "two.sided" = 1-alpha/2,
+      "less" =  1-alpha,
+      "greater" = 1-alpha
+    )
+  }
 
   se_val = sqrt(q1/(n1*p1) + q2/(n2*p2))
   CINT = phi * exp(c(-1,1)*z_mult*se_val)
@@ -225,32 +407,6 @@ calc_risk_ratio = function(p1,p2,n1,n2,
        CINT = CINT)
 }
 
-calc_prop_dif = function(p1,p2,n1,n2,
-                           conf_level = .95,
-                           conf = .975,
-                         alternative){
-  # Normal Approx.
-  prop_dif <- p1 - p2
-  # proportion se
-  prop_se <- sqrt((p1*(1-p1))/n1 + (p2*(1-p2))/n2)
-  z_mult = qnorm(conf)
-  q1 = 1-p1
-  q2 = 1-p2
-  ESTIMATE = prop_dif
-  names(ESTIMATE) = "difference in proportions"
 
-  CINT = prop_dif - c(-1,1)*(qnorm(conf) * prop_se)
-  if(alternative == "less"){
-    CINT[1] = -Inf
-  }
-
-  if(alternative == "greater"){
-    CINT[2] = Inf
-  }
-  attr(CINT, "conf.level") <- conf_level
-
-  list(ESTIMATE = ESTIMATE,
-       CINT = CINT)
-}
 
 
