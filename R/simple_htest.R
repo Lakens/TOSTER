87435875ffaf76fdaf719da3592dc5703467d3c8
arcaldwell49 <arcaldwell49@gmail.com>
2024-04-26 15:23:37 -0500

    Update simple_htest.R

--- R/simple_htest.R
+++ R/simple_htest.R
@@ -51,7 +51,7 @@ simple_htest <- function(x, ...,
                                          "greater",
                                          "equivalence",
                                          "minimal.effect"),
-                         mu = 0,
+                         mu = NULL,
                          alpha = 0.05){
   UseMethod("simple_htest")
 }
@@ -70,11 +70,18 @@ simple_htest.default = function(x,
                                                 "greater",
                                                 "equivalence",
                                                 "minimal.effect"),
-                                mu = 0,
+                                mu = NULL,
                                 alpha = 0.05,
                                 ...) {
  alternative = match.arg(alternative)
  test = match.arg(test)
+ if(is.null(mu)){
+   if(test == "brunner_munzel"){
+     mu = 0.5
+   } else{
+     mu = 0
+   }
+ }
 
  if(alternative %in% c("equivalence","minimal.effect")){
 
