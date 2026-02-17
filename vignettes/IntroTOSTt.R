60622a22c9e0b445c4b8d2dafb504233130c0ed8
Aaron Caldwell <arcaldwell49@gmail.com>
2026-02-17 13:39:41 -0600

    Add htest vignette

--- vignettes/IntroTOSTt.R
+++ vignettes/IntroTOSTt.R
@@ -7,6 +7,7 @@ library(TOSTER)
 library(ggplot2)
 library(ggdist)
 
+
 ## ----tostplots,echo=FALSE, message = FALSE, warning = FALSE, fig.show='hold'----
 
 ggplot() +
@@ -130,12 +131,15 @@ theme_tidybayes() +
     axis.ticks.y = element_blank()
   )
 
+
 ## -----------------------------------------------------------------------------
 data('sleep')
 data('iris')
 
+
 ## -----------------------------------------------------------------------------
 head(sleep)
+
 
 ## -----------------------------------------------------------------------------
 res1 = t_TOST(formula = extra ~ group,
@@ -148,12 +152,14 @@ res1a = t_TOST(x = subset(sleep, group==1)$extra,
                y = subset(sleep, group==2)$extra,
                eqb = .5)
 
+
 ## -----------------------------------------------------------------------------
 # Simple htest approach
 res1b = simple_htest(formula = extra ~ group,
                      data = sleep,
                      mu = .5,  # equivalence bound
                      alternative = "e")  # "e" for equivalence
+
 
 ## -----------------------------------------------------------------------------
 # Comprehensive t_TOST output
@@ -162,24 +168,30 @@ print(res1)
 # Concise htest output
 print(res1b)
 
+
 ## ----fig.width=6, fig.height=6------------------------------------------------
 plot(res1, type = "simple", layout = "combined")
+
 
 ## ----fig.width=6, fig.height=6, eval=TRUE-------------------------------------
 # Shade the 90% and 95% CI areas
 plot(res1, type = "cd",
      ci_shades = c(.9, .95))
 
+
 ## ----fig.width=6, fig.height=6------------------------------------------------
 plot(res1, type = "c",
      ci_lines = c(.9, .95))
 
+
 ## ----fig.width=6, fig.height=6------------------------------------------------
 plot(res1, type = "tnull")
+
 
 ## ----eval = FALSE-------------------------------------------------------------
 # describe(res1)
 # describe_htest(res1b)
+
 
 ## -----------------------------------------------------------------------------
 # For paired tests, use separate vectors rather than formula notation
@@ -197,6 +209,7 @@ res2b = simple_htest(
   alternative = "e")
 res2b
 
+
 ## -----------------------------------------------------------------------------
 res3 = t_TOST(x = iris$Sepal.Length,
               y = iris$Sepal.Width,
@@ -212,6 +225,7 @@ res3a = simple_htest(
   alternative = "e"
 )
 res3a
+
 
 ## -----------------------------------------------------------------------------
 res_met = t_TOST(x = iris$Sepal.Length,
@@ -229,9 +243,11 @@ res_metb = simple_htest(x = iris$Sepal.Length,
                        alternative = "minimal.effect")
 res_metb
 
+
 ## ----eval = FALSE-------------------------------------------------------------
 # describe(res_met)
 # describe_htest(res_metb)
+
 
 ## ----error=TRUE---------------------------------------------------------------
 try({
@@ -250,12 +266,14 @@ test = t_TOST(
 test
 })
 
+
 ## -----------------------------------------------------------------------------
 res4 = t_TOST(x = iris$Sepal.Length,
               hypothesis = "EQU",
               eqb = c(5.5, 8.5),  # lower and upper bounds
               smd_ci = "t")
 res4
+
 
 ## -----------------------------------------------------------------------------
 res_tsum = tsum_TOST(
@@ -268,11 +286,14 @@ res_tsum = tsum_TOST(
 
 res_tsum
 
+
 ## ----fig.width=6, fig.height=6------------------------------------------------
 plot(res_tsum)
 
+
 ## -----------------------------------------------------------------------------
 describe(res_tsum)
+
 
 ## -----------------------------------------------------------------------------
 power_t_TOST(n = NULL,
---
+++ vignettes/hypothesis_testing.R
@@ -0,0 +1,149 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+
+## ----setup--------------------------------------------------------------------
+library(TOSTER)
+
+
+## -----------------------------------------------------------------------------
+simple_htest(extra ~ group,
+             data = sleep,
+             mu = 0,
+             alternative = "two.sided")
+
+
+## -----------------------------------------------------------------------------
+t.test(extra ~ group, data = sleep)
+
+
+## -----------------------------------------------------------------------------
+simple_htest(extra ~ group,
+             data = sleep,
+             test = "wilcox.test",
+             mu = 0,
+             alternative = "two.sided")
+
+
+## -----------------------------------------------------------------------------
+simple_htest(extra ~ group,
+             data = sleep,
+             mu = 2,
+             alternative = "equivalence")
+
+
+## -----------------------------------------------------------------------------
+simple_htest(extra ~ group,
+             data = sleep,
+             mu = c(-1, 3),
+             alternative = "equivalence")
+
+
+## -----------------------------------------------------------------------------
+simple_htest(extra ~ group,
+             data = sleep,
+             mu = -1,
+             alternative = "greater")
+
+
+## -----------------------------------------------------------------------------
+simple_htest(extra ~ group,
+             data = sleep,
+             mu = 1,
+             alternative = "greater")
+
+
+## -----------------------------------------------------------------------------
+brunner_munzel(extra ~ group, data = sleep)
+
+
+## -----------------------------------------------------------------------------
+z_cor_test(mtcars$mpg, mtcars$hp,
+           method = "pearson",
+           alternative = "two.sided",
+           null = 0)
+
+
+## -----------------------------------------------------------------------------
+set.seed(2101)
+boot_t_test(extra ~ group,
+            data = sleep,
+            mu = 0,
+            alternative = "two.sided",
+            R = 999)
+
+
+## -----------------------------------------------------------------------------
+set.seed(8251)
+perm_t_test(extra ~ group,
+            data = sleep,
+            mu = 0,
+            alternative = "two.sided",
+            R = 999)
+
+
+## -----------------------------------------------------------------------------
+tost_res <- t_TOST(extra ~ group,
+                   data = sleep,
+                   eqb = 2)
+as_htest(tost_res)
+
+
+## -----------------------------------------------------------------------------
+res_t <- simple_htest(extra ~ group, data = sleep, mu = 0)
+df_htest(res_t)
+
+
+## -----------------------------------------------------------------------------
+res_equiv <- simple_htest(extra ~ group, data = sleep,
+                          mu = 2, alternative = "equivalence")
+res_wilcox <- simple_htest(extra ~ group, data = sleep,
+                           mu = 0, test = "wilcox.test")
+
+combined <- rbind(
+  df_htest(res_t),
+  df_htest(res_equiv),
+  df_htest(res_wilcox)
+)
+combined
+
+
+## -----------------------------------------------------------------------------
+describe_htest(res_t)
+
+
+## -----------------------------------------------------------------------------
+describe_htest(res_equiv)
+
+
+## ----fig.width=6, fig.height=3------------------------------------------------
+plot_htest_est(res_t)
+
+
+## ----fig.width=6, fig.height=3------------------------------------------------
+plot_htest_est(res_equiv)
+
+
+## ----fig.width=6, fig.height=3------------------------------------------------
+plot_htest_est(res_equiv, describe = FALSE)
+
+
+## ----fig.width=6, fig.height=3------------------------------------------------
+# Run equivalence test
+result <- simple_htest(extra ~ group,
+                       data = sleep,
+                       mu = 2,
+                       alternative = "equivalence")
+
+# Tabulate
+df_htest(result)
+
+# Describe
+describe_htest(result)
+
+# Visualize
+plot_htest_est(result)
+
